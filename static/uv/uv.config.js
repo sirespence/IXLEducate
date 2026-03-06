1fe6d4fe8e7e542364362935e2c580fd8b93cad1
sirespence <162833998+sirespence@users.noreply.github.com>
2026-03-06 17:03:43 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/tiw/~/',
-    bare: 'https://bare.benrogo.net',
+    bare: 'https://tomp.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

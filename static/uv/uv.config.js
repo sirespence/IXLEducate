694543ed7da4082edaf65e35e858056872c920aa
sirespence <162833998+sirespence@users.noreply.github.com>
2026-03-06 14:21:03 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://bare.benroberts.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',

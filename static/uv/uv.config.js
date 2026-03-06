322c216028de05792453705b1253eb9de224f25b
sirespence <162833998+sirespence@users.noreply.github.com>
2026-03-06 16:48:22 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/tiw/',
     bare: 'https://bare.benrogo.net',
     encodeUrl: ultraviolet.codec.xor.encode,
     decodeUrl: ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
